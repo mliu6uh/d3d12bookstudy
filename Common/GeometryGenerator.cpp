d4289b27da19fa9c41c03112fb4f0b0afd5b6582
Min Liu <mliu@newpark.com>
2024-04-08 00:12:32 -0500

    x-y-z plan improved with back, bottom and left faces shown ok

--- Common/GeometryGenerator.cpp
+++ Common/GeometryGenerator.cpp
@@ -57,7 +57,7 @@ GeometryGenerator::MeshData GeometryGenerator::CreateBox(float width, float heig
 	//v[22] = Vertex(+w2, +h2, +d2, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f);
 	//v[23] = Vertex(+w2, -h2, +d2, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
 
-	meshData.Vertices.assign(&v[0], &v[24]);
+	meshData.Vertices.assign(&v[0], &v[12]);
  
 	//
 	// Create the indices.
@@ -66,20 +66,20 @@ GeometryGenerator::MeshData GeometryGenerator::CreateBox(float width, float heig
 	uint32 i[36];
 
 	// Fill in the back face index data
-	i[0] = 0; i[1] = 1; i[2] = 2;
-	i[3] = 0; i[4] = 2; i[5] = 3;
+	i[0] = 0; i[1] = 2; i[2] = 1;
+	i[3] = 0; i[4] = 3; i[5] = 2;
 
 	//// Fill in the top face index data
 	//i[12] = 8; i[13] = 9; i[14] = 10;
 	//i[15] = 8; i[16] = 10; i[17] = 11;
 
 	// Fill in the bottom face index data
-	i[6] = 4; i[7] = 5; i[8] = 6;
-	i[9] = 4; i[10] = 6; i[11] = 7;
+	i[6] = 4; i[7] = 6; i[8] = 5;
+	i[9] = 4; i[10] = 7; i[11] = 6;
 
 	// Fill in the left face index data
-	i[12] = 8; i[13] = 9; i[14] = 10;
-	i[15] = 8; i[16] = 10; i[17] = 11;
+	i[12] = 8; i[13] = 10; i[14] = 9;
+	i[15] = 8; i[16] = 11; i[17] = 10;
 
 	//// Fill in the right face index data
 	//i[30] = 20; i[31] = 21; i[32] = 22;
